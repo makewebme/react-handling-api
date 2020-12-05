a91755f83b8d8097867af128962050ec23e2c06a
makeweb <makewebme.russia@gmail.com>
2020-12-05 18:29:38 +0300

    App state 5 (improved cancel requests)

--- src/PostData.js
+++ src/PostData.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { delayRandomly, throwRandomly } from './helpers'
 
@@ -7,27 +7,18 @@ const PostData = ({ id }) => {
   // Данные полученного поста
   const [ data, setData ] = useState(null)
 
-  // Место для хранения свежайшего AbortController'а
-  const lastAbortController = useRef()
-
   // Дергаем, когда меняется ID
   useEffect(() => {
     // Опустошаем данные
     setData(null)
 
-    // Если уже есть сохранённый AbortController, то отменяем его
-    if (lastAbortController.current) {
-      lastAbortController.current.abort()
-    }
-
-    // Создаем AbortController для нового запроса и сохраняем его
-    const curAbortController = new AbortController()
-    lastAbortController.current = curAbortController
+    // Создаем AbortController для нового запроса
+    const abortController = new AbortController();
 
     // Делаем запрос на получение поста с текущим ID
     // взятым из state'а компонента выше и сохранем промис
     const curPromise = fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-      signal: curAbortController.signal
+      signal: abortController.signal
     })
       .then(res => res.json())
       .then(async data => {
@@ -38,9 +29,18 @@ const PostData = ({ id }) => {
 
     // Обрабатываем ответ сервера
     curPromise.then(
-      res => setData(res),
+      res => {
+        if (abortController.signal.aborted) {
+          return
+        }
+
+        setData(res)
+      },
       e => console.warn('fetch failure', e)
     )
+
+    // Отменяем запрос, если компонент был за-unmount'чен
+    return () => abortController.abort()
   }, [ id ])
 
   // Отображаем данные поста или лоадер
